11ca582eb9850371f0167ed69d9a48d775ff17c1
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-04-01 14:54:50 +0200

    added first product to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+niceCat = Product.create(title: "Quality Kitty",
+ subtitle: "Learn App Code", image_name: "cat.JPEG",
+ price: "4.99", sku: "kittyOne", info: "cute cat", download_url: "https://www.dropbox.com/s/hznfjoe5y714o9q/cat.jpeg?dl=0",
+ details: "A really nice cat", description: %{<p>It's a cat</p>
+ })
+
