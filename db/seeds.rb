af8e746f2db8d3378f54ee0a8e4ff7a8e9c0bf08
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-04-01 15:07:58 +0200

    added a products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,8 @@ niceCat = Product.create(title: "Quality Kitty",
  details: "A really nice cat", description: %{<p>It's a cat</p>
  })
 
+niceDog = Product.create(title: "Quality Dog",
+ subtitle: "Learn App Code", image_name: "dog.jpg",
+ price: "4.99", sku: "dogOne", info: "cute pup", download_url: "https://s3-us-west-2.amazonaws.com/rails-learnapp-dog/dog.jpg",
+ details: "A really nice dog", description: %{<p>It's a dog</p>
+ })
